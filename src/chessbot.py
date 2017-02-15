b1299a784ac8612ede83f58fc42b602169f458af
Alex Kruger <alex.kruger@csiro.au>
2017-02-15 17:26:17 +1100

    added alpha beta pruning

--- src/chessbot.py
+++ src/chessbot.py
@@ -16,7 +16,7 @@ class ChessBot:
 				best_move = move
 		return best_move
 
-	def score_move(self, board, depth):
+	def score_move(self, board, alpha=0, beta=1, depth):
 		moves = list(board.legal_moves)
 		if depth == 0 or len(moves) == 0:
 			return self.eval_move(board)
@@ -25,24 +25,29 @@ class ChessBot:
 		best_move = None
 		for move in moves:
 			board.push(move)
-			score = self.score_move(board, depth-1)
+			score = self.score_move(board, alpha, beta, depth-1)
 			board.pop()
 			if best_score == None:
 				best_score = score
 				best_move = move
-			elif max_player: #MAX
+			if max_player: #MAX
 				if score > best_score:
 					best_score = score
 					best_move = move
+				alpha = max([alpha, score])
 			else: #MIN
 				if score < best_score:
 					best_score = score
 					best_move = move
+				beta = min([beta, score])
+			if beta <= alpha:
+				#prune
+				break
 		return best_score
  
 	def eval_move(self, board):
 		moves = list(board.legal_moves)
-		score = 0
+		score = -1
 		if len(moves) == 0:
 			if board.result() in ['1-0', '0-1']:
 				score = 1
@@ -53,12 +58,22 @@ class ChessBot:
 			batch_x = np.zeros(shape=(len(moves), 8, 8, 12), dtype=np.int8)
 			for i, move in enumerate(moves):
 				board.push(move)
+				result = board.result()
+				if result in ['1-0', '0-1']:
+					score = 0
+					board.pop()
+					break
+				elif result == '1/2-1/2':
+					score = 0.5
+					board.pop()
+					break
 				batch_x[i] = self.board_to_matrix(board)
 				board.pop()
-			out = model.predict_proba(batch_x, verbose=0)
-			scores = [s[0] for s in out]
-			max_score = max(scores)
-			score = 1 - max_score
+			if score == -1:
+				out = model.predict_proba(batch_x, verbose=0)
+				scores = [s[0] for s in out]
+				max_score = max(scores)
+				score = 1 - max_score
 		if not board.turn == self.player:
 			return score
 		else:
