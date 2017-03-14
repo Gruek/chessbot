c45d8f158e6d84f2ba5c3c696a7c29c81639f7ef
Alex Kruger <alex.kruger@csiro.au>
2017-03-14 17:20:43 +1100

    added caching

--- src/chessbot.py
+++ src/chessbot.py
@@ -3,14 +3,22 @@ from model import model, WEIGHTS_FILE
 import numpy as np
 
 class ChessBot:
+	def __init__(self):
+		self.max_cache = 2
+		self.cache = [{} * self.max_cache]
+
 	def best_move(self, board, depth=5, eval=False):
+		#clean old cache
+		self.cache.insert(0, {})
+		self.cache = self.cache[:self.max_cache]
+
 		self.player = board.turn
 		score, move = self.score_move(board, depth)
 		if eval:
 			print(score)
 		return move
 
-	def score_move(self, board, depth, alpha=0, beta=1):
+	def score_move(self, board, depth, alpha=0, beta=1, max_breadth=5):
 		moves = list(board.legal_moves)
 		if depth == 0 or len(moves) == 0:
 			score = self.eval_move(board)
@@ -26,7 +34,7 @@ class ChessBot:
 		#get top 5 moves
 		scores = self.score_moves(moves, board)
 		scores.sort(key = lambda x: x['score'], reverse=True)
-		moves = [score['move'] for score in scores[:5]]
+		moves = [score['move'] for score in scores[:max_breadth]]
 
 		#go deeper
 		for move in moves:
@@ -60,24 +68,31 @@ class ChessBot:
 		elif result == '1/2-1/2':
 			score = 0.5
 		else:
-			#eval board by looking at next move
-			batch_x = np.zeros(shape=(len(moves), 8, 8, 12), dtype=np.int8)
-			for i, move in enumerate(moves):
-				board.push(move)
-				result = board.result()
-				if result in ['1-0', '0-1']:
-					score = 0
+			board_hash = board.board_fen() + int(board.turn)
+			cached = self.from_cache(board_hash)
+			if cached:
+				score = cached
+			else:
+				#eval board by looking at next move
+				batch_x = np.zeros(shape=(len(moves), 8, 8, 12), dtype=np.int8)
+				for i, move in enumerate(moves):
+					board.push(move)
+					result = board.result()
+					if result in ['1-0', '0-1']:
+						score = 0
+						board.pop()
+						break
+					batch_x[i] = self.board_to_matrix(board)
 					board.pop()
-					break
-				batch_x[i] = self.board_to_matrix(board)
-				board.pop()
-			if score == -1:
-				out = model.predict(batch_x, verbose=0)
-				scores = [s[0] for s in out]
-				max_score = max(scores)
-				score = 1 - max_score
-			if score > 0.5 and board.can_claim_threefold_repetition():
-				score = 0.5
+				if score == -1:
+					out = model.predict(batch_x, verbose=0)
+					scores = [s[0] for s in out]
+					max_score = max(scores)
+					score = 1 - max_score
+				if score > 0.5 and board.can_claim_threefold_repetition():
+					score = 0.5
+				#cache score
+				self.cache[0][board_hash] = score
 		if not board.turn == self.player:
 			return score
 		else:
@@ -117,3 +132,12 @@ class ChessBot:
 						x = 7-x
 					state[x][y][piece_idx] = 1
 		return state
+
+	def from_cache(self, board_hash):
+		for cachen in self.cache:
+			if board_hash in cachen:
+				ret = cachen[board_hash]
+				#move to primary cache
+				self.cache[0][board_hash] = ret
+				return ret
+		return None
